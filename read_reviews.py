9b7227ea1b62a8b6b51c1ab58d33dca64b093e09
Fox Liu <lgxxabc@gmail.com>
2020-11-03 09:14:41 -0600

    calculate the average length of reviews

--- read_reviews.py
+++ read_reviews.py
@@ -5,10 +5,16 @@ with open('reviews.txt', 'r') as f:
 	for line in f:
 		data.append(line)
 		count += 1
-		if count % 10000 == 0:		# Print every 10000 lines
+		if count % 10000 == 0:		# Print every 10000 lines as a progress bar
 			print(len(data))
-print(len(data))		#1000000
+print('There is a total of', len(data), 'lines')		#1000000
 
-print(data[0])
-print('------------------------------------------------')
-print(data[1])
+# print(data[0])
+# print('------------------------------------------------')
+# print(data[1])
+
+# count the average length of lines
+sum_len = 0
+for d in data:
+	sum_len += len(d)
+print('The average length of reviews is:', sum_len/len(data))
