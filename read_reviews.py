efb852c1974711219f8b36f3711248dd1438ae53
Fox Liu <lgxxabc@gmail.com>
2020-11-03 20:45:38 -0600

    add filter

--- read_reviews.py
+++ read_reviews.py
@@ -5,16 +5,25 @@ with open('reviews.txt', 'r') as f:
 	for line in f:
 		data.append(line)
 		count += 1
-		if count % 10000 == 0:		# Print every 10000 lines as a progress bar
+		if count % 100000 == 0:		# Print every 100000 lines as a progress bar
 			print(len(data))
 print('There is a total of', len(data), 'lines')		#1000000
 
-# print(data[0])
-# print('------------------------------------------------')
-# print(data[1])
+print(data[0])
+print('------------------------------------------------')
+print(data[1])
 
 # count the average length of lines
 sum_len = 0
-for d in data:
+for d in data:		# d is string(1000000)
 	sum_len += len(d)
 print('The average length of reviews is:', sum_len/len(data))
+
+# filter
+new = []
+for d in data:
+	if len(d) < 100:
+		new.append(d)
+print('There are', len(new), 'lines less than 100 characters')
+# check the first line in new.
+print(new[0])
